6614ab02e140c12d7e0f2c7eccfae567beb3cbaf
jiyahan <jiyahan@test.com>
2024-08-15 11:52:59 +0800

    fix some build errors.

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\github_repo\OptimizedGestureDetector\example"
+export "FLUTTER_ROOT=C:\Users\jiyah\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\Chart\OptimizedGestureDetector\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
